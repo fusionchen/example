11f5b1e0fe8f9dcbc6b1662342e81d61c7817f05
fusionchen <fusion.chen@gmail.com>
2015-04-09 00:48:07 +0800

    add new example

---
+++ app/scripts/newFile.js
@@ -0,0 +1,7 @@
+http://jsfiddle.net/ctz358f6/805/
+  http://blog.pixelingene.com/2011/08/progressive-reveal-animations-in-svg-using-a-svgclippath/
+    http://fau.jh.gg/D3BST/
+      http://jsfiddle.net/2pdxz/
+        http://stackoverflow.com/questions/27744701/collapsible-tree-with-combined-horizontal-and-vertical-layouts
+          http://jsfiddle.net/78Eqh/1/
+            http://jsfiddle.net/bGa48/
